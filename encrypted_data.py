a2a24fe62fda388248874abd3c3a37d9c73a2629
修观 <xiuguan.gp@alibaba-inc.com>
2016-05-04 15:02:56 +0800

    python

--- encrypted_data.py
+++ encrypted_data.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
+import json
 import struct
 import base64
 from Crypto.Cipher import AES
 
 # data = '{"source_uid": "123312", "source": "qq", "source_name": "hello"}'
-data = '{"source_uid" : "0", "source" : "self_account"}'
+data = json.dumps({"source_uid" : "0", "source" : "self_account"}) # JSON 格式字符串
 data = struct.pack(">I",len(data)) + data
 
 class prpcrypt():
@@ -23,7 +25,7 @@ class prpcrypt():
 		ciphertext = cryptor.encrypt(new_text)
 		return ciphertext
 
-aes_key = '这里放API_Secret'
+aes_key = '这里放App_Secret'
 data = prpcrypt(aes_key).encrypt(data)
 data = base64.b64encode( data )
 print data
